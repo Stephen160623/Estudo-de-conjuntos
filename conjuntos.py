6eaae82b4edf4388630d9fa8c13f09e4fed13cae
Stephen160623 <136847798+Stephen160623@users.noreply.github.com>
2023-06-23 16:01:42 -0300

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,23 @@
+'''
+Programa: Conjuntos
+Descrição: Esse programa cria dois conjuntos de exemplo e adiciona uma variável ao segundo.
+Autor: Eu
+Data: 23/06/2023
+Versão: 0.0.1
+'''
+# Atribuição de variáveis
+
+lista = [1,5,2,3,8,6,7,8,8,5]
+conjunto_1 = set(lista)
+conjunto_2 = set([1,5,3,8,3,3,2,5,6])
+
+# Entrada de dados
+
+#Processamento de dados
+
+conjunto_2.add(49)
+
+# Saída de dados
+
+print(conjunto_1)
+print(conjunto_2)
